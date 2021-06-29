eb189bb7cd9045ae22e57a6a5eea3c5b5c99dbed
Thomas Visset <thmvisset31@gmail.com>
2021-06-29 03:36:10 +0200

    add or remove items in cart

--- 11-food-order-app/01-starting-project/src/App.js
+++ 11-food-order-app/01-starting-project/src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-import CartProvider from "./components/store/cart-provider";
+import CartProvider from "./components/store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
