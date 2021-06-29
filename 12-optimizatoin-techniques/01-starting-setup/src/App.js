7976c1b54b36c0de0461008d548b464ba2a8e841
Thomas Visset <thmvisset31@gmail.com>
2021-06-29 23:24:05 +0200

    prettier

--- 12-optimizatoin-techniques/01-starting-setup/src/App.js
+++ 12-optimizatoin-techniques/01-starting-setup/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [] = useState()
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <p>This is New!</p>
     </div>
   );
 }
