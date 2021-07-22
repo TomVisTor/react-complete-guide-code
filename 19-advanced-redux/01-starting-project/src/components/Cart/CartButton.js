f148d7e4b14d88a4ee58eb31f017cb5b01d3f3d6
Thomas Visset <thmvisset31@gmail.com>
2021-07-22 14:21:49 +0200

    add Redux

---
+++ 19-advanced-redux/01-starting-project/src/components/Cart/CartButton.js
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { uiActions } from '../../store/ui-slice';
+import classes from './CartButton.module.css';
+
+const CartButton = (props) => {
+  const cartQuantity = useSelector(state => state.cart.totalQuantity)
+  const dispatch = useDispatch()
+
+  const toogleCartHandler = () => {
+    dispatch(uiActions.toogle())
+  }
+
+  return (
+    <button className={classes.button} onClick={toogleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
