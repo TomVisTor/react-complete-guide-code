b9cfcc813cac61adc4db49762f6edc23764f03c2
Thomas Visset <thmvisset31@gmail.com>
2021-07-22 14:59:47 +0200

    add .env to fetch data on firebase

--- 19-advanced-redux/01-starting-project/src/App.js
+++ 19-advanced-redux/01-starting-project/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,14 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(process.env.REACT_APP_FIRE_BASE_REDUX_PROJECT_GET, {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
