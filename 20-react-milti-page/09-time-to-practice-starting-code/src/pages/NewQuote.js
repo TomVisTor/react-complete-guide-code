28323449652096033e0163130c35e1e8f717030d
Thomas Visset <thmvisset31@gmail.com>
2021-07-27 16:13:24 +0200

    add api to fetch from FireBase

--- 20-react-milti-page/09-time-to-practice-starting-code/src/pages/NewQuote.js
+++ 20-react-milti-page/09-time-to-practice-starting-code/src/pages/NewQuote.js
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+
+import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
+
 const NewQuote = () => {
-  return <h1>New Quote Page</h1>
+  const { sendRequest, status } = useHttp(addQuote);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
+  const addQuoteHandler = (quoteData) => {
+    sendRequest(quoteData);
+  };
+
+  return (
+    <QuoteForm
+      isLoading={status === "pending"}
+      onAddQuote={addQuoteHandler}
+    ></QuoteForm>
+  );
 };
 
 export default NewQuote;
